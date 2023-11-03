3ab1a7f47d7a548a088ad85138129f98f2c107a7
Ovidijus M <mykovidijus@gmail.com>
2023-11-03 16:46:33 +0200

    Created Modal

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.scss';
 import Colorfliper from './views/colorfliper/colorFliper';
 import Counter from './views/counter/counter';
-import './assets/css/animations.css';
-import './assets/css/all.scss';
+import './assets/scss/animations.scss';
+import './assets/scss/all.scss';
 import './views/counter/counter.scss';
 import {
   BrowserRouter as Router,
@@ -16,27 +16,37 @@ import Sidebar from './components/sidebar/sidebar';
 import menu from './assets/img/icons/menu.svg';
 import Navbar from './components/sidebar/sidebar';
 import Home from './views/home/home';
+import Modal from './components/modal/modal';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.sidebarComponent = React.createRef();
+    this.modalComponent = React.createRef();
   }
   
   toggleSidebar = () => {
     this.sidebarComponent.current.toggleSidebar();
   }
+
+  openModal=()=> {
+    this.modalComponent.current.openModal();
+  }
   
   render(){
     return (
       <Router>
+        <Modal ref={this.modalComponent}></Modal>
         <div className="App">
           {/* <Navbar></Navbar> */}
           <Sidebar ref={this.sidebarComponent}></Sidebar>
           <div className="content">
             <button onClick={this.toggleSidebar}>
               <img src={menu} alt="" />
+            </button>
+            <button onClick={this.openModal}>
+              Modal
             </button>
             <Switch>
               <Route path="/views/colorfliper">
---
+++ src/components/modal/modal.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './modal.scss';
+import link from '../../assets/img/icons/link.svg';
+
+
+export default class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false,
+            headerText: 'Connection Integration',
+            img: '',
+            bodyText: 'A place to manage all the bots, & other apps',
+            bodyText2: 'Syncs data between apps & create powerful automated workflows',
+            linkType: true
+        }
+    }
+
+    setLinkType(value){
+        this.setState({
+            linkType: value
+        });
+    }
+    openModal=()=>{
+        this.setState({
+            showModal: true
+        })
+    }
+    closeModal=()=>{
+        this.setState({
+            showModal: false
+        })
+    }
+    render(){
+        return (
+            <div className={("modal-holder " + (this.state.showModal ? 'show' : ''))}>
+                <div className="modal">
+                    <div className="header">
+                        <img src={link} alt="" />
+                        <h6>{ this.state.headerText }</h6>
+                        <button onClick={this.closeModal}>x</button>
+                    </div>
+                
+                    <div className="body">
+                        <div className='button-holder'>
+                            <button onClick={() => this.setLinkType(true)} className={(this.state.linkType ? 'enabled' : '')}>Quick Link</button>
+                            <button onClick={() => this.setLinkType(false)} className={(!this.state.linkType ? 'enabled' : '')}>Manual Link</button>
+                        </div>
+                        <div className="graphic">
+
+                        </div>
+                        {this.state.linkType===true &&
+                        <p>
+                            { this.state.bodyText }
+                        </p>
+                        }
+                        {this.state.linkType===false &&
+                        <p>
+                            { this.state.bodyText2 }
+                        </p>
+                        }
+                    </div>
+                    <div className="footer">
+                        <button className='submit'>Connect Integration</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}  

179ee84d2efaa17be16101b5afbdde70256bf9bb
Ovidijus M <mykovidijus@gmail.com>
2023-11-06 22:09:22 +0200

    Created video background

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -3,11 +3,46 @@ import React from 'react';
 import './home.scss';
 import MenuLinks from '../../components/menuLinks/menuLinks';
 
+
+import video from '../../assets/video/glitch.mp4';
+
+
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      play: true
+    }
+  }
+  
+  getVideo = elem => {
+    this.video = elem
+  }
+
+  toggleVideo = () => {
+    if(!this.state.play){
+      this.video.play();
+    } else {
+      this.video.pause();
+    }
+    this.setState({
+      play: !this.state.play
+    })
+  };
+
+
   render(){
     return (
       <div className="fade-in home" >
-        <MenuLinks></MenuLinks>
+        <div className="holder">
+          <MenuLinks></MenuLinks>
+          <video ref={this.getVideo}  className='videoTag' autoPlay loop muted>
+              <source src={video} type='video/mp4' />
+          </video>
+        </div>
+          <button onClick={this.toggleVideo} className='videoToggle'>
+            {this.state.play ? 'Play' : 'Stop'}
+          </button>
       </div>
     );
   }
